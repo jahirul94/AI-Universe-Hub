9b72e155a933cac9eacfed5b0b52b9dacc85dbf1
jahirul94 <jahirulislam41185@gmail.com>
2023-03-02 22:40:26 +0600

    sppiner section upto complate

--- js-file/app.js
+++ js-file/app.js
@@ -8,19 +8,29 @@ const loadFetch =()=>{
 
 const displayData =(data)=>{
  const mainContainer =document.getElementById('main-container')
+ mainContainer.classList.add('main-comtainer')
  data.tools.forEach(singleAiTools => {
    console.log(singleAiTools)
   mainContainer.innerHTML += ` <div class="card">
   <img src="${singleAiTools.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Features</h5>
-     <li>${singleAiTools.features[0]}</li>
-     <li>${singleAiTools.features[1]}</li>
-     <li>${singleAiTools.features[2]}</li>
+       <ol class ="ps-4"> 
+        <li>${singleAiTools.features[0]}</li>
+        <li>${singleAiTools.features[1]}</li>
+        <li>${singleAiTools.features[2]}</li>
+       </ol>
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-  </ul>`;
+   <div class="list-group ps-3 py-2">
+      <h5 class="card-title">${singleAiTools.name}</h5>
+       <div class="d-flex">
+          <img height="18px" width ="18px" src="icons/calendar.png" alt="">
+          <h6 class ="ps-2">${singleAiTools.published_in}</h6>
+       </div>
+   </div>   
+  
+  
+  `;
 
 
 
@@ -28,7 +38,12 @@ const displayData =(data)=>{
  }); 
 }
 
-
-
-
-loadFetch()
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+     loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+}
