a8422fa82c5458921a752d2fb4a2c68d413e3442
jahirul94 <jahirulislam41185@gmail.com>
2023-03-03 22:32:35 +0600

    aded responsive style for mobile

--- js-file/modal.js
+++ js-file/modal.js
@@ -9,7 +9,6 @@ const loadModalFetch =(id)=>{
 
 
 const displayModal =(data)=>{
-  console.log(data)
  const modalHeader = document.getElementById('modal-header');
   modalHeader.innerText =`${data.description}`;
    
@@ -21,9 +20,55 @@ const displayModal =(data)=>{
    <p>${data.input_output_examples[0].output}</p>
   `;
 
-//   price section 
-  
 
+//  //   price section basic
+const basics = getElementByIds('basic') ;
+const basicPrice = data.pricing[0];
+  const div = document.createElement('div');
+  basics.innerText ='';
+  if(basicPrice.price === '0' || basicPrice.price === 'No cost'){
+        div.innerText = `Free Of Cost`;
+  }
+  else{
+        div.innerText = `${basicPrice.price},
+        ${basicPrice.plan}`
+    }
+
+    basics.appendChild(div)
+
+  //  pro
+const pro = getElementByIds('pro') ;
+const proPrice = data.pricing[1];
+  const div1 = document.createElement('div');
+  pro.innerText ='';
+  if(proPrice.price === '0' || proPrice.price === 'No cost'){
+        div1.innerText = 'Free Of Cost';
+  }
+  else{
+        div1.innerText = `${proPrice.price},
+        ${proPrice.plan}`
+    }
+
+    pro.appendChild(div1)
+
+    // enterprice
+const enterprice = getElementByIds('enterprice') ;
+const enterpricePrice = data.pricing[2];
+  const div2 = document.createElement('div');
+  enterprice.innerText ='';
+  if(enterpricePrice.price === '0' || enterpricePrice.price === 'No cost'){
+        div2.innerText = 'Free Of Cost';
+  }
+  else{
+        div2.innerText = `${enterpricePrice.price},
+        ${enterpricePrice.plan}`
+    }
+
+    enterprice.appendChild(div2)
+ 
+ 
+
+ 
 
 
 
